107a88603851432bc893c716dd3bbd49ff13ff8f
swedk <127571222+poseww@users.noreply.github.com>
2023-11-19 01:04:05 +0700

    Update routes.py

--- meet15a/application/routes.py
+++ meet15a/application/routes.py
@@ -120,10 +120,64 @@ def create_post():
 
 
 
-@app.route('/signup')
-def signup():
-    form = SignUpForm()
-    return render_template('signup.html', title='SignUp', form=form)
+@app.route('/forgot_password', methods=['GET', 'POST'])
+def forgot_password():
+    form = ForgotPasswordForm()
+
+    if form.validate_on_submit():
+        email = form.email.data
+
+        # Check if email exists in the user data
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            # Process the password reset (simplified example, in reality, you would handle this securely)
+            flash('Reset password link has been sent to your email.', 'success')
+            return redirect(url_for('login'))
+
+        else:
+            flash("Email not found. Please check again.")
+
+        # No need for db.session.commit() in this case unless you are making changes to the database.
+
+    return render_template('forgot_password.html', title='forgot_password', form=form)
+    
+@app.route('/edit_post/<int:post_id>', methods=['GET', 'POST'])
+@login_required
+def edit_post(post_id):
+    form = EditPostForm()
+
+    post = Post.query.get(post_id)
+    if form.validate_on_submit():
+        post.caption = form.caption.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('profile', username=current_user.username))
+
+    elif request.method == 'GET':
+        form.caption.data = post.caption
+
+    return render_template('edit_post.html', title='Edit Post', form=form, post=post)
+
+
+@app.route('/reset_password', methods=['GET', 'POST'])
+@login_required
+def reset_password():
+    form = ResetPasswordForm()
+
+    if form.validate_on_submit():
+        user = User.query.get(current_user.id)
+        if user.password != form.old_password.data:
+            flash("ur old password is wrong")
+        user.password = form.new_password.data
+        user.password_Confirm = form.confirm_new_password.data
+
+        db.session.commit()
+        flash("password has been reset", 'success')
+        return redirect(url_for('profile', username=current_user.username))
+    
+    return render_template('reset_pass.html', title='Reset Password', form=form)
+
 
 @app.route('/about')
 def about():
